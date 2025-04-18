960027894555b94132db819e8c393161e3ca5bfa
Tausif Rahman <69134814+tawsif0@users.noreply.github.com>
2025-04-18 11:12:53 +0600

    updated index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <Router>
+    <Router basename="/">
         <App />
     </Router>
 );
